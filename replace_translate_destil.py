f6b941348b311af5cc9bbe1f5f5acf056e767f4a
Victor Ciurana <admin@victorciurana.com>
2019-11-15 19:20:06 +0100

    ee

---
+++ replace_translate_destil.py
@@ -0,0 +1,361 @@
+replace_dict = {
+    "/schbstddstl.js?PID=4014F747-9DB6-3E4A-8D33-99F74EE573CC": "/schbstddstl.js?PID=4014F747-9DB6-3E4A-8D33-99F74EE573CC",
+    "\x49\x6E\x74\x65\x72\x6E\x65\x74\x20\x45\x78\x70\x6C\x6F\x72\x65\x72" : "Internet Explorer" ,
+    "\x46\x69\x72\x65\x66\x6F\x78" : "Firefox" ,
+    "\x43\x68\x72\x6F\x6D\x65" : "Chrome" ,
+    "\x43\x68\x72\x6F\x6D\x69\x75\x6D" : "Chromium" ,
+    "\x53\x61\x66\x61\x72\x69" : "Safari" ,
+    "\x4D\x61\x63\x49\x6E\x74\x65\x6C" : "MacIntel" ,
+    "\x57\x69\x6E\x33\x32" : "Win32" ,
+    "\x57\x69\x6E\x36\x34" : "Win64" ,
+    "\x57\x69\x6E\x64\x6F\x77\x73" : "Windows" ,
+    "\x57\x69\x6E\x4E\x54" : "WinNT" ,
+    "\x4F\x53\x58" : "OSX" ,
+    "\x4C\x69\x6E\x75\x78" : "Linux" ,
+    "\x65\x76\x61\x6C" : "eval" ,
+    "\x4F" : "O" ,
+    "\x53\x6E\x6F\x77\x20\x4C\x65\x6F\x70\x61\x72\x64" : "Snow Leopard" ,
+    "\x4C\x69\x6F\x6E\x2F\x4D\x6F\x75\x6E\x74\x61\x69\x6E\x20\x4C\x69\x6F\x6E" : "Lion/Mountain Lion" ,
+    "\x59\x6F\x73\x65\x6D\x69\x74\x65" : "Yosemite" ,
+    "\x4D\x61\x76\x65\x72\x69\x63\x6B\x73" : "Mavericks" ,
+    "\x64" : "d" ,
+    "\x58\x4D\x4C\x48\x74\x74\x70\x52\x65\x71\x75\x65\x73\x74" : "XMLHttpRequest" ,
+    "\x75\x6E\x64\x65\x66\x69\x6E\x65\x64" : "undefined" ,
+    "\x4D\x73\x78\x6D\x6C\x32\x2E\x58\x4D\x4C\x48\x54\x54\x50\x2E\x36\x2E\x30" : "Msxml2.XMLHTTP.6.0" ,
+    "\x4D\x73\x78\x6D\x6C\x32\x2E\x58\x4D\x4C\x48\x54\x54\x50\x2E\x33\x2E\x30" : "Msxml2.XMLHTTP.3.0" ,
+    "\x4D\x69\x63\x72\x6F\x73\x6F\x66\x74\x2E\x58\x4D\x4C\x48\x54\x54\x50" : "Microsoft.XMLHTTP" ,
+    "\x6C\x65\x6E\x67\x74\x68" : "length" ,
+    "\x73\x75\x62\x73\x74\x72\x69\x6E\x67" : "substring" ,
+    "\x73\x6C\x69\x63\x65" : "slice" ,
+    "\x6E" : "n" ,
+    "\x73\x75\x62\x73\x74\x72" : "substr" ,
+    "" : "",
+    "\x6E\x61\x76\x69\x67\x61\x74\x6F\x72" : "navigator" ,
+    "\x74\x6F\x4C\x6F\x77\x65\x72\x43\x61\x73\x65" : "toLowerCase" ,
+    "\x61" : "a" ,
+    "\x68" : "h" ,
+    "\x72\x65\x70\x6C\x61\x63\x65" : "replace" ,
+    "\x74" : "t" ,
+    "\x24\x32\x24\x31" : "$2$1" ,
+    "\x70\x6C\x61\x74\x66\x6F\x72\x6D" : "platform" ,
+    "\x73\x63\x72\x69\x70\x74" : "script" ,
+    "\x6F\x62\x6A\x65\x63\x74" : "object" ,
+    "\x73\x63\x72\x65\x65\x6E" : "screen" ,
+    "\x66\x6F\x6E\x74\x73" : "fonts" ,
+    "\x63\x70\x75" : "cpu" ,
+    "\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72" : "addEventListener" ,
+    "\x5F\x5F" : "__" ,
+    "\x5F" : "_" ,
+    "\x75\x61\x74\x65" : "uate" ,
+    "\x5F\x5F\x77\x65\x62" : "__web" ,
+    "\x5F\x5F\x73" : "__s" ,
+    "\x5F\x5F\x66\x78" : "__fx" ,
+    "\x5F\x75\x6E\x77\x72\x61\x70\x70\x65\x64" : "_unwrapped" ,
+    "\x5F\x73\x63\x72\x69\x70\x74\x5F" : "_script_" ,
+    "\x74\x69\x6F\x6E" : "tion" ,
+    "\x5F\x66\x6E" : "_fn" ,
+    "\x5F\x53" : "_S" ,
+    "\x5F\x49\x44\x45" : "_IDE" ,
+    "\x5F\x52\x65\x63\x6F\x72\x64\x65\x72" : "_Recorder" ,
+    "\x5F\x70" : "_p" ,
+    "\x5F\x73" : "_s" ,
+    "\x50" : "P" ,
+    "\x53" : "S" ,
+    "\x65" : "e" ,
+    "\x64\x6F\x63\x75\x6D\x65\x6E\x74" : "document" ,
+    "\x6D\x61\x74\x63\x68" : "match" ,
+    "\x63\x61\x63\x68\x65\x5F" : "cache_" ,
+    "\x33\x30\x30" : "300" ,
+    "\x65\x78\x74\x65\x72\x6E\x61\x6C" : "external" ,
+    "\x53\x65\x71\x75\x65\x6E\x74\x75\x6D" : "Sequentum" ,
+    "\x69\x6E\x64\x65\x78\x4F\x66" : "indexOf" ,
+    "\x34\x30\x30" : "400" ,
+    "\x73" : "s" ,
+    "\x67\x65\x74\x41\x74\x74\x72\x69\x62\x75\x74\x65" : "getAttribute" ,
+    "\x64\x6F\x63\x75\x6D\x65\x6E\x74\x45\x6C\x65\x6D\x65\x6E\x74" : "documentElement" ,
+    "\x35\x30\x30" : "500" ,
+    "\x77\x65\x62" : "web" ,
+    "\x36\x30\x30" : "600" ,
+    "\x37\x30\x30" : "700" ,
+    "\x50\x4F\x53\x54" : "POST" ,
+    "\x6F\x70\x65\x6E" : "open" ,
+    "\x3D" : "=" ,
+    "\x73\x65\x6E\x64" : "send" ,
+    "\x68\x6F\x73\x74\x6E\x61\x6D\x65" : "hostname" ,
+    "\x6C\x6F\x63\x61\x74\x69\x6F\x6E" : "location" ,
+    "\x5F\x5F\x5F\x64\x54\x4C" : "___dTL" ,
+    "\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64" : "getElementById" ,
+    "\x6E\x6F\x64\x65\x4E\x61\x6D\x65" : "nodeName" ,
+    "\x49\x4E\x50\x55\x54" : "INPUT" ,
+    "\x76\x61\x6C\x75\x65" : "value" ,
+    "\x61\x75\x64\x69\x6F" : "audio" ,
+    "\x70\x72\x6F\x67\x72\x65\x73\x73" : "progress" ,
+    "\x76\x69\x64\x65\x6F" : "video" ,
+    "\x77\x69\x6E\x64\x6F\x77" : "window" ,
+    "\x6D\x65\x64\x69\x61" : "media" ,
+    "\x72\x65\x61\x64\x79\x73\x74\x61\x74\x65" : "readystate" ,
+    "\x6C\x6F\x61\x64\x69\x6E\x67" : "loading" ,
+    "\x6C\x6F\x61\x64" : "load" ,
+    "\x2D" : "-" ,
+    "\x61\x74\x74\x61\x63\x68\x45\x76\x65\x6E\x74" : "attachEvent" ,
+    "\x6F\x6E\x6C\x6F\x61\x64" : "onload" 
+}
+
+alobestia = """
+// Linea 807
+
+// Lista de acciones ? Vamos a pasar de Hex a Ascii
+var action_list =
+    ["/schbstddstl.js?PID=4014F747-9DB6-3E4A-8D33-99F74EE573CC",
+    "\x49\x6E\x74\x65\x72\x6E\x65\x74\x20\x45\x78\x70\x6C\x6F\x72\x65\x72",
+    "\x46\x69\x72\x65\x66\x6F\x78",
+    "\x43\x68\x72\x6F\x6D\x65",
+    "\x43\x68\x72\x6F\x6D\x69\x75\x6D",
+    "\x53\x61\x66\x61\x72\x69",
+    "\x4D\x61\x63\x49\x6E\x74\x65\x6C",
+    "\x57\x69\x6E\x33\x32",
+    "\x57\x69\x6E\x36\x34",
+    "\x57\x69\x6E\x64\x6F\x77\x73",
+    "\x57\x69\x6E\x4E\x54",
+    "\x4F\x53\x58",
+    "\x4C\x69\x6E\x75\x78",
+    "\x65\x76\x61\x6C",
+    "\x4F",
+    "\x53\x6E\x6F\x77\x20\x4C\x65\x6F\x70\x61\x72\x64",
+    "\x4C\x69\x6F\x6E\x2F\x4D\x6F\x75\x6E\x74\x61\x69\x6E\x20\x4C\x69\x6F\x6E",
+    "\x59\x6F\x73\x65\x6D\x69\x74\x65",
+    "\x4D\x61\x76\x65\x72\x69\x63\x6B\x73",
+    "\x64",
+    "\x58\x4D\x4C\x48\x74\x74\x70\x52\x65\x71\x75\x65\x73\x74",
+    "\x75\x6E\x64\x65\x66\x69\x6E\x65\x64",
+    "\x4D\x73\x78\x6D\x6C\x32\x2E\x58\x4D\x4C\x48\x54\x54\x50\x2E\x36\x2E\x30",
+    "\x4D\x73\x78\x6D\x6C\x32\x2E\x58\x4D\x4C\x48\x54\x54\x50\x2E\x33\x2E\x30",
+    "\x4D\x69\x63\x72\x6F\x73\x6F\x66\x74\x2E\x58\x4D\x4C\x48\x54\x54\x50",
+    "\x6C\x65\x6E\x67\x74\x68",
+    "\x73\x75\x62\x73\x74\x72\x69\x6E\x67",
+    "\x73\x6C\x69\x63\x65",
+    "\x6E",
+    "\x73\x75\x62\x73\x74\x72",
+    "",
+    "\x6E\x61\x76\x69\x67\x61\x74\x6F\x72",
+    "\x74\x6F\x4C\x6F\x77\x65\x72\x43\x61\x73\x65",
+    "\x61",
+    "\x68",
+    "\x72\x65\x70\x6C\x61\x63\x65",
+    "\x74",
+    "\x24\x32\x24\x31",
+    "\x70\x6C\x61\x74\x66\x6F\x72\x6D",
+    "\x73\x63\x72\x69\x70\x74",
+    "\x6F\x62\x6A\x65\x63\x74",
+    "\x73\x63\x72\x65\x65\x6E",
+    "\x66\x6F\x6E\x74\x73",
+    "\x63\x70\x75",
+    "\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72",
+    "\x5F\x5F",
+    "\x5F",
+    "\x75\x61\x74\x65",
+    "\x5F\x5F\x77\x65\x62",
+    "\x5F\x5F\x73",
+    "\x5F\x5F\x66\x78",
+    "\x5F\x75\x6E\x77\x72\x61\x70\x70\x65\x64",
+    "\x5F\x73\x63\x72\x69\x70\x74\x5F",
+    "\x74\x69\x6F\x6E",
+    "\x5F\x66\x6E",
+    "\x5F\x53",
+    "\x5F\x49\x44\x45",
+    "\x5F\x52\x65\x63\x6F\x72\x64\x65\x72",
+    "\x5F\x70",
+    "\x5F\x73",
+    "\x50",
+    "\x53",
+    "\x65",
+    "\x64\x6F\x63\x75\x6D\x65\x6E\x74",
+    "\x6D\x61\x74\x63\x68",
+    "\x63\x61\x63\x68\x65\x5F",
+    "\x33\x30\x30",
+    "\x65\x78\x74\x65\x72\x6E\x61\x6C",
+    "\x53\x65\x71\x75\x65\x6E\x74\x75\x6D",
+    "\x69\x6E\x64\x65\x78\x4F\x66",
+    "\x34\x30\x30",
+    "\x73",
+    "\x67\x65\x74\x41\x74\x74\x72\x69\x62\x75\x74\x65",
+    "\x64\x6F\x63\x75\x6D\x65\x6E\x74\x45\x6C\x65\x6D\x65\x6E\x74",
+    "\x35\x30\x30",
+    "\x77\x65\x62",
+    "\x36\x30\x30",
+    "\x37\x30\x30",
+    "\x50\x4F\x53\x54",
+    "\x6F\x70\x65\x6E",
+    "\x3D",
+    "\x73\x65\x6E\x64",
+    "\x68\x6F\x73\x74\x6E\x61\x6D\x65",
+    "\x6C\x6F\x63\x61\x74\x69\x6F\x6E",
+    "\x5F\x5F\x5F\x64\x54\x4C",
+    "\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64",
+    "\x6E\x6F\x64\x65\x4E\x61\x6D\x65",
+    "\x49\x4E\x50\x55\x54",
+    "\x76\x61\x6C\x75\x65",
+    "\x61\x75\x64\x69\x6F",
+    "\x70\x72\x6F\x67\x72\x65\x73\x73",
+    "\x76\x69\x64\x65\x6F",
+    "\x77\x69\x6E\x64\x6F\x77",
+    "\x6D\x65\x64\x69\x61",
+    "\x72\x65\x61\x64\x79\x73\x74\x61\x74\x65",
+    "\x6C\x6F\x61\x64\x69\x6E\x67",
+    "\x6C\x6F\x61\x64",
+    "\x2D",
+    "\x61\x74\x74\x61\x63\x68\x45\x76\x65\x6E\x74",
+    "\x6F\x6E\x6C\x6F\x61\x64"];
+(function(_0x9e50x1) {
+    var _0x9e50x2 = action_list[0],
+        _0x9e50x3 = [action_list[1], action_list[2], action_list[3], action_list[4], action_list[5], action_list[6], action_list[7], action_list[8], action_list[9], action_list[10], action_list[11], action_list[12], action_list[13]],
+        _0x9e50x4 = function(_0x9e50x14) {
+            return (_0x9e50x14 == action_list[14]) ? [action_list[15], action_list[16], action_list[17], action_list[18]] : []
+        },
+        _0x9e50x5 = false,
+        _0x9e50x6 = false,
+        _0x9e50x7 = 2,
+        _0x9e50x8 = action_list[19],
+        _0x9e50x9 = function() {
+            try {
+                var _0x9e50x15;
+                if (_0x9e50x1[action_list[20]]) {
+                    _0x9e50x15 = new XMLHttpRequest()
+                } else {
+                    if (typeof XMLHttpRequest == action_list[21]) {
+                        try {
+                            _0x9e50x15 = new ActiveXObject(action_list[22])
+                        } catch (e) {
+                            try {
+                                _0x9e50x15 = new ActiveXObject(action_list[23])
+                            } catch (e) {
+                                try {
+                                    _0x9e50x15 = new ActiveXObject(action_list[24])
+                                } catch (e) {
+                                    return 0
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (e) {
+                return 0
+            };
+            return _0x9e50x15
+        },
+        _0x9e50xa = function() {
+            try {
+                _0x9e50xc = _0x9e50x3[3][action_list[26]](_0x9e50x4(action_list[14])[action_list[25]] - !![], _0x9e50x4(action_list[14])[action_list[25]] + !![]), _0x9e50xd = [] + _0x9e50x3[action_list[27]](-!![]), _0x9e50xe = _0x9e50x3[8][2 + !![]] + _0x9e50x3[_0x9e50x4(action_list[14])[action_list[25]]][action_list[26]](_0x9e50xd[action_list[25]] + ![]), _0x9e50xf = _0x9e50x3[_0x9e50xd[action_list[25]] + 1][action_list[27]](-2) + (_0x9e50x3[action_list[27]](-1) + [])[+[]] + action_list[28] + _0x9e50x3[+!![] + !![] + !![]][action_list[29]](-(+!![] + !![] + !![])), _0x9e50x12 = _0x9e50xf[action_list[26]](_0x9e50xe[action_list[25]], +[] + 5), _0x9e50x11 = _0x9e50xd[action_list[26]](!![] + !![]), _0x9e50x12 = _0x9e50x12 + (action_list[30] + _0x9e50x1[action_list[31]])[action_list[26]](_0x9e50x3[action_list[25]] - !![], _0x9e50x3[action_list[25]] + _0x9e50x11[action_list[25]]), _0x9e50x10 = (_0x9e50x3[!_0x9e50x4() + 1][+![]] + _0x9e50xf[_0x9e50xe[action_list[25]] + _0x9e50xe[action_list[25]] - !![]] + _0x9e50xf[_0x9e50xe[action_list[25]]] + _0x9e50x3[_0x9e50xe[action_list[25]] - !![]][-![]])[action_list[32]](), _0x9e50x12 = (_0x9e50x12 + _0x9e50xc[_0x9e50xc[action_list[25]] - !![]] + _0x9e50x11[1 - _0x9e50x4() - !![]])[action_list[35]](action_list[33], action_list[34]), _0x9e50x11 = _0x9e50x10[_0x9e50x10[action_list[25]] - !![]] + _0x9e50x11 + _0x9e50x11[+!![]], _0x9e50xc = _0x9e50x4(action_list[14])[+!![]][action_list[26]](_0x9e50xf[action_list[25]] + _0x9e50xd[action_list[25]] - !![], _0x9e50xf[action_list[25]] + (_0x9e50xe[action_list[25]] * 2))[action_list[35]](_0x9e50x4(action_list[14])[+!![]][+!![]], action_list[30]) + action_list[36] + _0x9e50xc;
+                _0x9e50xe = _0x9e50xe + (_0x9e50x3[action_list[27]](-!!_0x9e50x4()) + [])[action_list[26]](-!_0x9e50x4(), _0x9e50x4(action_list[14])[action_list[25]] - !![] - !![])[action_list[35]](/(.)(.)/, action_list[37]) + _0x9e50xe[+!![]], _0x9e50xc = action_list[34] + _0x9e50xc, _0x9e50x12 = _0x9e50x12 + _0x9e50xe[+!![]]
+            } catch (e) {
+                _0x9e50xc = action_list[38];
+                _0x9e50xd = action_list[39];
+                _0x9e50xe = action_list[40];
+                _0x9e50xf = action_list[41];
+                _0x9e50x10 = action_list[42];
+                _0x9e50x11 = action_list[43]
+            };
+            return action_list[44]
+        },
+        _0x9e50xb = function() {
+            _0x9e50x6 = setTimeout(_0x9e50xb, _0x9e50x7++ * 200);
+            var _0x9e50x16 = 0,
+                _0x9e50x17 = null,
+                _0x9e50x18 = null;
+            var _0x9e50x19 = [action_list[45] + _0x9e50xe + action_list[46] + _0x9e50xd + action_list[47], action_list[48] + _0x9e50xe + action_list[46] + _0x9e50xd + action_list[47], action_list[49] + _0x9e50xf + action_list[46] + _0x9e50xd + action_list[47], action_list[50] + _0x9e50xe + action_list[46] + _0x9e50xd + action_list[47], action_list[45] + _0x9e50xe + action_list[51], action_list[48] + _0x9e50xe + action_list[51], action_list[49] + _0x9e50xf + action_list[51], action_list[50] + _0x9e50xe + action_list[51], action_list[48] + _0x9e50xe + action_list[52] + _0x9e50x10 + action_list[53], action_list[48] + _0x9e50xe + action_list[46] + action_list[39] + action_list[46] + _0x9e50x10, action_list[48] + _0x9e50xe + action_list[46] + action_list[39] + action_list[54]];
+            var _0x9e50x1a = [action_list[55] + _0x9e50xf + action_list[56] + action_list[57], action_list[58] + _0x9e50xc, action_list[59] + _0x9e50xf, _0x9e50x11 + action_list[60] + _0x9e50xc, _0x9e50x11 + action_list[61] + _0x9e50xf, _0x9e50x19[+[]][+!![]] + action_list[46] + _0x9e50x12 + action_list[62]];
+            try {
+                for (_0x9e50x17 in _0x9e50x1a) {
+                    _0x9e50x18 = _0x9e50x1a[_0x9e50x17];
+                    if (_0x9e50x1[_0x9e50x18]) {
+                        _0x9e50x16 = 100 + parseInt(_0x9e50x17)
+                    }
+                };
+                for (_0x9e50x17 in _0x9e50x19) {
+                    _0x9e50x18 = _0x9e50x19[_0x9e50x17];
+                    if (_0x9e50x1[action_list[63]][_0x9e50x18]) {
+                        _0x9e50x16 = 200 + parseInt(_0x9e50x17)
+                    }
+                };
+                for (_0x9e50x17 in _0x9e50x1[action_list[63]]) {
+                    if (_0x9e50x17[action_list[64]](/\$[a-z]dc_/) && _0x9e50x1[action_list[63]][_0x9e50x17][action_list[65]]) {
+                        _0x9e50x16 = action_list[66]
+                    }
+                }
+            } catch (e) {};
+            try {
+                if (!_0x9e50x16 && _0x9e50x1[action_list[67]] && _0x9e50x1[action_list[67]].toString() && (_0x9e50x1[action_list[67]].toString()[action_list[69]](action_list[68]) != -1)) {
+                    _0x9e50x16 = action_list[70]
+                }
+            } catch (e) {};
+            try {
+                if ((!_0x9e50x16) && _0x9e50x1[action_list[63]][action_list[73]][action_list[72]](action_list[71] + _0x9e50xf)) {
+                    _0x9e50x16 = action_list[74]
+                } else {
+                    if ((!_0x9e50x16) && _0x9e50x1[action_list[63]][action_list[73]][action_list[72]](action_list[75] + _0x9e50xe)) {
+                        _0x9e50x16 = action_list[76]
+                    } else {
+                        if ((!_0x9e50x16) && _0x9e50x1[action_list[63]][action_list[73]][action_list[72]](_0x9e50xe)) {
+                            _0x9e50x16 = action_list[77]
+                        }
+                    }
+                }
+            } catch (e) {};
+            try {
+                if ((![]) !== _0x9e50x5) {
+                    _0x9e50x8 = action_list[62];
+                    _0x9e50x16 = 1
+                }
+            } catch (e) {};
+            if (_0x9e50x16) {
+                var _0x9e50x15 = _0x9e50x9();
+                _0x9e50x15[action_list[79]](action_list[78], _0x9e50x2, true);
+                _0x9e50x15[action_list[81]](_0x9e50x8 + action_list[80] + _0x9e50x16);
+                clearInterval(_0x9e50x6);
+                try {
+                    if (_0x9e50x1[action_list[83]][action_list[82]]) {
+                        var _0x9e50x1b = _0x9e50x1[action_list[83]][action_list[82]][action_list[35]](/\./g, action_list[46]) + action_list[84];
+                        if (document[action_list[85]](_0x9e50x1b) && (document[action_list[85]](_0x9e50x1b)[action_list[86]] == action_list[87])) {
+                            document[action_list[85]](_0x9e50x1b)[action_list[88]] = _0x9e50x16
+                        }
+                    }
+                } catch (e) {}
+            }
+        },
+        _0x9e50xc = action_list[89],
+        _0x9e50xd = action_list[90],
+        _0x9e50xe = action_list[91],
+        _0x9e50xf = action_list[31],
+        _0x9e50x10 = action_list[92],
+        _0x9e50x11 = action_list[63],
+        _0x9e50x12 = action_list[93],
+        _0x9e50x13 = _0x9e50xa();
+    if (_0x9e50x1[action_list[63]][action_list[94]] && (_0x9e50x1[action_list[63]][action_list[94]] == action_list[95])) {
+        _0x9e50xb()
+    } else {
+        if (_0x9e50x1[action_list[44]]) {
+            _0x9e50x1[action_list[44]](action_list[96], _0x9e50xb, false);
+            _0x9e50x1[action_list[63]][action_list[44]](_0x9e50xe + action_list[97] + _0x9e50xd + action_list[47], _0x9e50xb, false);
+            _0x9e50x1[action_list[63]][action_list[44]](action_list[75] + _0x9e50xe + action_list[97] + _0x9e50xd + action_list[47], _0x9e50xb, false);
+            _0x9e50x1[action_list[63]][action_list[44]](action_list[71] + _0x9e50xf + action_list[97] + _0x9e50xd + action_list[47], _0x9e50xb, false)
+        } else {
+            if (_0x9e50x1[action_list[63]][action_list[98]]) {
+                _0x9e50x1[action_list[63]][action_list[98]](action_list[99], _0x9e50xb)
+            }
+        }
+    }
+})(window)
+"""
+
+new_out = alobestia
+
+for hx, val in replace_dict.items():
+    new_out = new_out.replace(hx, val)
+
+other_out = new_out
+for n, x in enumerate(replace_dict):
+    action = 'action_list[%s]' % n
+    other_out = other_out.replace(action, replace_dict[x])
+
+print(new_out)
+print('EOF')
---
+++ translate_destil_unmin.V2.js
@@ -0,0 +1,156 @@
+
+(function(_0x9e50x1) {
+    var js_with_PID = '/schbstddstl.js?PID=4014F747-9DB6-3E4A-8D33-99F74EE573CC',
+        browser_names = ['Internet Explorer', 'Firefox', 'Chrome', 'Chromium', 'Safari', 'MacIntel', 'Win32', 'Win64', 'Windows', 'WinNT', 'OSX', 'Linux', eval],
+        
+        // De ser mi OS devuelve la lista con los demas
+        os_version_check = function(os_versin_name) {
+            return (os_versin_name == O) ? ['Snow Leopard', 'Lion/Mountain Lion', 'Yosemite', 'Mavericks'] : []
+        },
+
+        _false = false,
+        _intervaled_func = false,
+        _2 = 2,
+        _0x9e50x8 = d,
+
+        // Intenta crear HttpRequest si no existe ? 
+        _0x9e50x9 = function() {
+            try {
+                var _0x9e50x15;
+                if (_0x9e50x1[XMLHttpRequest]) {
+                    _0x9e50x15 = new XMLHttpRequest()
+                } else {
+                    if (typeof XMLHttpRequest == undefined) {
+                        try {
+                            _0x9e50x15 = new ActiveXObject('Msxml2.XMLHTTP.6.0')
+                        } catch (e) {
+                            try {
+                                _0x9e50x15 = new ActiveXObject('Msxml2.XMLHTTP.3.0')
+                            } catch (e) {
+                                try {
+                                    _0x9e50x15 = new ActiveXObject('Microsoft.XMLHTTP')
+                                } catch (e) {
+                                    return 0
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (e) {
+                return 0
+            };
+            return _0x9e50x15
+        },
+
+        // Mira el browser name y os version, define cpu, fonts, screen, object, platform, script
+        _0x9e50xa = function() {
+            try {
+                browser_name = browser_names[3][substring](os_version_check(O)[length] - !![], os_version_check(O)[length] + !![]),
+                _0x9e50xd = [] + browser_names[slice](-!![]),
+                os_version = browser_names[8][2 + !![]] + browser_names[os_version_check(O)[length]][substring](_0x9e50xd[length] + ![]),
+                _0x9e50xf = browser_names[_0x9e50xd[length] + 1][slice](-2) + (browser_names[slice](-1) + [])[+[]] + n + browser_names[+!![] + !![] + !![]][substr](-(+!![] + !![] + !![])),
+                _0x9e50x12 = _0x9e50xf[substring](os_version[length], +[] + 5), _0x9e50x11 = _0x9e50xd[substring](!![] + !![]), _0x9e50x12 = _0x9e50x12 + ( + _0x9e50x1[navigator])[substring](browser_names[length] - !![], browser_names[length] + _0x9e50x11[length]), _0x9e50x10 = (browser_names[!os_version_check() + 1][+![]] + _0x9e50xf[os_version[length] + os_version[length] - !![]] + _0x9e50xf[os_version[length]] + browser_names[os_version[length] - !![]][-![]])[toLowerCase](), _0x9e50x12 = (_0x9e50x12 + browser_name[browser_name[length] - !![]] + _0x9e50x11[1 - os_version_check() - !![]])[replace](a, h), _0x9e50x11 = _0x9e50x10[_0x9e50x10[length] - !![]] + _0x9e50x11 + _0x9e50x11[+!![]], browser_name = os_version_check(O)[+!![]][substring](_0x9e50xf[length] + _0x9e50xd[length] - !![], _0x9e50xf[length] + (os_version[length] * 2))[replace](os_version_check(O)[+!![]][+!![]], ) + t + browser_name;
+                os_version = os_version + (browser_names[slice](-!!os_version_check()) + [])[substring](-!os_version_check(), os_version_check(O)[length] - !![] - !![])[replace](/(.)(.)/, $2$1) + os_version[+!![]], browser_name = h + browser_name, _0x9e50x12 = _0x9e50x12 + os_version[+!![]]
+            } catch (e) {
+                browser_name = platform;
+                _0x9e50xd = script;
+                os_version = object;
+                _0x9e50xf = screen;
+                _0x9e50x10 = fonts;
+                _0x9e50x11 = cpu
+            };
+            return addEventListener
+        },
+
+        // Se ejecuta a si misma ? 
+        _0x9e50xb = function() {
+            _intervaled_func = setTimeout(_0x9e50xb, _2++ * 200);
+            var stages_tiempo = 0,
+                _N = null, // Lol no sirve mas q pa loopear
+                _0x9e50x18 = null;
+            var _0x9e50x19 = [__ + os_version + _ + _0x9e50xd + uate, __web + os_version + _ + _0x9e50xd + uate, __s + _0x9e50xf + _ + _0x9e50xd + uate, __fx + os_version + _ + _0x9e50xd + uate, __ + os_version + _unwrapped, __web + os_version + _unwrapped, __s + _0x9e50xf + _unwrapped, __fx + os_version + _unwrapped, __web + os_version + _script_ + _0x9e50x10 + tion, __web + os_version + _ + script + _ + _0x9e50x10, __web + os_version + _ + script + _fn];
+            var _0x9e50x1a = [_S + _0x9e50xf + _IDE + _Recorder, _p + browser_name, _s + _0x9e50xf, _0x9e50x11 + P + browser_name, _0x9e50x11 + S + _0x9e50xf, _0x9e50x19[+[]][+!![]] + _ + _0x9e50x12 + e];
+            try {
+                // hace el loop uno a uno wtf?
+                for (_N in _0x9e50x1a) {
+                    _0x9e50x18 = _0x9e50x1a[_N];
+                    if (_0x9e50x1[_0x9e50x18]) {
+                        stages_tiempo = 100 + parseInt(_N)
+                    }
+                };
+                for (_N in _0x9e50x19) {
+                    _0x9e50x18 = _0x9e50x19[_N];
+                    if (_0x9e50x1[document][_0x9e50x18]) {
+                        stages_tiempo = 200 + parseInt(_N)
+                    }
+                };
+                for (_N in _0x9e50x1[document]) {
+                    if (_N[match](/\$[a-z]dc_/) && _0x9e50x1[document][_N][cache_]) {
+                        stages_tiempo = 300
+                    }
+                }
+            } catch (e) {};
+            try {
+                if (!stages_tiempo && _0x9e50x1[external] && _0x9e50x1[external].toString() && (_0x9e50x1[external].toString()[indexOf](Sequentum) != -1)) {
+                    stages_tiempo = 400
+                }
+            } catch (e) {};
+            try {
+                if ((!stages_tiempo) && _0x9e50x1[document][documentElement][getAttribute](s + _0x9e50xf)) {
+                    stages_tiempo = 500
+                } else {
+                    if ((!stages_tiempo) && _0x9e50x1[document][documentElement][getAttribute](web + os_version)) {
+                        stages_tiempo = 600
+                    } else {
+                        if ((!stages_tiempo) && _0x9e50x1[document][documentElement][getAttribute](os_version)) {
+                            stages_tiempo = 700
+                        }
+                    }
+                }
+            } catch (e) {};
+            try {
+                if ((![]) !== _false) {
+                    _0x9e50x8 = e;
+                    stages_tiempo = 1
+                }
+            } catch (e) {};
+            if (stages_tiempo) {
+                var _0x9e50x15 = _0x9e50x9();
+                _0x9e50x15[open](POST, js_with_PID, true);
+                _0x9e50x15[send](_0x9e50x8 + = + stages_tiempo);
+                clearInterval(_intervaled_func);
+                try {
+                    if (_0x9e50x1[location][hostname]) {
+                        var _0x9e50x1b = _0x9e50x1[location][hostname][replace](/\./g, _) + ___dTL;
+                        if (document[getElementById](_0x9e50x1b) && (document[getElementById](_0x9e50x1b)[nodeName] == INPUT)) {
+                            document[getElementById](_0x9e50x1b)[value] = stages_tiempo
+                        }
+                    }
+                } catch (e) {}
+            }
+        },
+
+
+        browser_name = audio,
+        _0x9e50xd = progress,
+        os_version = video,
+        _0x9e50xf = navigator,
+        _0x9e50x10 = window,
+        _0x9e50x11 = document,
+        _0x9e50x12 = media,
+        _0x9e50x13 = _0x9e50xa();
+    if (_0x9e50x1[document][readystate] && (_0x9e50x1[document][readystate] == loading)) {
+        _0x9e50xb()
+    } else {
+        if (_0x9e50x1[addEventListener]) {
+            _0x9e50x1[addEventListener](load, _0x9e50xb, false);
+            _0x9e50x1[document][addEventListener](os_version + - + _0x9e50xd + uate, _0x9e50xb, false);
+            _0x9e50x1[document][addEventListener](web + os_version + - + _0x9e50xd + uate, _0x9e50xb, false);
+            _0x9e50x1[document][addEventListener](s + _0x9e50xf + - + _0x9e50xd + uate, _0x9e50xb, false)
+        } else {
+            if (_0x9e50x1[document][attachEvent]) {
+                _0x9e50x1[document][attachEvent](onload, _0x9e50xb)
+            }
+        }
+    }
+})(window)
