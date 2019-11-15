e19e52b61d730f6605aa3995d70f131f0ef39819
Victor Ciurana <admin@victorciurana.com>
2019-11-15 18:02:16 +0100

    wee

--- sandbox.py
+++ sandbox.py
@@ -8,7 +8,10 @@ from bs4 import BeautifulSoup
 
 # Url
 url = 'https://www.infojobs.net/'
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+"""
+USER_AGENT = ir a navegador abrir consola y poner navigator.userAgent
+"""
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
 headers = {'User-Agent': user_agent}
 # Request
 r = requests.get(url, headers=headers)      
